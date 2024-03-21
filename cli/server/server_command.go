76bda41ee8e4a2670f45a63549486c1cd1aead1f
xingchi.jin <xingchi.jin@harness.io>
2024-03-20 18:11:34 -0700

    Added Runner installation cli and make it a proper server.

---
+++ cli/server/server_command.go
@@ -0,0 +1,115 @@
+// Copyright 2021 Harness Inc. All rights reserved.
+// Use of this source code is governed by the PolyForm Shield 1.0.0 license
+// that can be found in the licenses directory at the root of this repository, also available at
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+
+package server
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/harness/runner/cli/server/handlers"
+	"github.com/harness/runner/delegateshell"
+	"github.com/harness/runner/delegateshell/client"
+	"github.com/harness/runner/delegateshell/delegate"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+type serverCommand struct {
+	envFile string
+}
+
+func (c *serverCommand) run(*kingpin.ParseContext) error {
+	// Load env file if exists
+	if c.envFile != "" {
+		loadEnvErr := godotenv.Load(c.envFile)
+		if loadEnvErr != nil {
+			logrus.WithError(loadEnvErr).Errorln("cannot load env file")
+		}
+	}
+
+	// Read configs into memory
+	loadedConfig, err := delegate.FromEnviron()
+	if err != nil {
+		logrus.WithError(err).Errorln("Load Runner config failed")
+	}
+
+	// create the http serverInstance.
+	serverInstance := Server{
+		Addr:     loadedConfig.Server.Bind,
+		Handler:  handlers.Handler(),
+		CAFile:   loadedConfig.Server.CACertFile, // CA certificate file
+		CertFile: loadedConfig.Server.CertFile,   // Server certificate PEM file
+		KeyFile:  loadedConfig.Server.KeyFile,    // Server key file
+		Insecure: loadedConfig.Server.Insecure,   // Skip server certificate verification
+	}
+
+	// trap the os signal to gracefully shutdown the http server.
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt)
+	defer func() {
+		signal.Stop(s)
+		cancel()
+	}()
+	go func() {
+		select {
+		case val := <-s:
+			logrus.Infof("received OS Signal to exit server: %s", val)
+			cancel()
+		case <-ctx.Done():
+			logrus.Infoln("received a done signal to exit server")
+		}
+	}()
+
+	logrus.Info("registering")
+
+	err = processHarnessTasks(ctx, &loadedConfig)
+	if err != nil {
+		cancel()
+		logrus.Error("Error starting polling tasks from Harness")
+	}
+
+	// TODO: INIT_SCRIPT feature, how to and where to
+	// run the setup checks / installation
+	// if loadedConfig.Server.SkipPrepareServer {
+	// 	logrus.Infoln("skipping prepare server eg install docker / git")
+	// } else {
+	// 	setup.PrepareSystem()
+	// }
+	// starts the http server.
+	err = serverInstance.Start(ctx)
+	if err == context.Canceled {
+		logrus.Infoln("program gracefully terminated")
+		return nil
+	}
+
+	if err != nil {
+		logrus.Errorf("program terminated with error: %s", err)
+	}
+
+	return err
+}
+
+func Register(app *kingpin.Application) {
+	c := new(serverCommand)
+
+	cmd := app.Command("server", "start the server").
+		Action(c.run)
+
+	cmd.Flag("env-file", "environment file").
+		Default(".env").
+		StringVar(&c.envFile)
+}
+
+func processHarnessTasks(ctx context.Context, config *delegate.Config) error {
+	requestsChan := make(chan *client.RunnerRequest, 3)
+	_, err := delegateshell.Start(ctx, config, requestsChan)
+	// router.process(requestChan)
+	return err
+}
--- delegateshell/delegate/config.go
+++ delegateshell/delegate/config.go
@@ -1,19 +1,36 @@
+// Copyright 2021 Harness Inc. All rights reserved.
+// Use of this source code is governed by the PolyForm Shield 1.0.0 license
+// that can be found in the licenses directory at the root of this repository, also available at
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+
 package delegate
 
 import (
+	"strings"
+
 	"github.com/kelseyhightower/envconfig"
 )
 
 // Sample config
 type Config struct {
-	Debug bool `envconfig:"DRONE_DEBUG"`
-	Trace bool `envconfig:"DRONE_TRACE"`
+	Debug bool `envconfig:"DEBUG"`
+	Trace bool `envconfig:"TRACE"`
 
 	Delegate struct {
-		AccountID       string `envconfig:"DRONE_DELEGATE_ACCOUNT_ID"`
-		AccountSecret   string `envconfig:"DRONE_DELEGATE_ACCOUNT_SECRET"`
-		ManagerEndpoint string `envconfig:"DRONE_DELEGATE_MANAGER_ENDPOINT"`
-		Name            string `envconfig:"DRONE_DELEGATE_NAME"`
+		AccountID       string `envconfig:"ACCOUNT_ID"`
+		DelegateToken   string `envconfig:"DELEGATE_TOKEN"`
+		Tags            string `envconfig:"DELEGATE_TAGS"`
+		ManagerEndpoint string `envconfig:"MANAGER_HOST_AND_PORT"`
+		Name            string `envconfig:"DELEGATE_NAME"`
+	}
+
+	Server struct {
+		Bind              string `envconfig:"HTTPS_BIND" default:":3000"`
+		CertFile          string `envconfig:"SERVER_CERT_FILE" default:"/tmp/certs/server-cert.pem"` // Server certificate PEM file
+		KeyFile           string `envconfig:"SERVER_KEY_FILE" default:"/tmp/certs/server-key.pem"`   // Server key PEM file
+		CACertFile        string `envconfig:"CLIENT_CERT_FILE" default:"/tmp/certs/ca-cert.pem"`     // CA certificate file
+		SkipPrepareServer bool   `envconfig:"SKIP_PREPARE_SERVER" default:"false"`                   // skip prepare server, install docker / git
+		Insecure          bool   `envconfig:"SERVER_INSECURE" default:"true"`                        // run in insecure mode
 	}
 }
 
@@ -25,4 +42,12 @@ func FromEnviron() (Config, error) {
 	}
 
 	return config, nil
+}
+
+func (c *Config) GetTags() []string {
+	tags := make([]string, 0)
+	for _, s := range strings.Split(c.Delegate.Tags, ",") {
+		tags = append(tags, strings.TrimSpace(s))
+	}
+	return tags
 }
