876f6d2f8663575dbd1375b3b912eb73d98d1dc0
Vinicius Calasans <vinicius.calasans@harness.io>
2024-07-16 15:58:59 -0300

    Add support for decoding secrets stored in base64 format

--- tasks/secrets/static.go
+++ tasks/secrets/static.go
@@ -2,6 +2,7 @@ package secrets
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 
 	"github.com/drone/go-task/task"
@@ -20,14 +21,25 @@ func (h *StaticSecretHandler) Handle(ctx context.Context, req *task.Request) tas
 	// TODO: support batch secrets
 	secretResponse := &task.Secret{}
 	if len(staticSecretSpec.Secrets) > 0 {
-		secretResponse.Value = staticSecretSpec.Secrets[0].Value
+		secret := staticSecretSpec.Secrets[0]
+		secretValue := secret.Value
+		if secret.Base64 == true {
+			decodedValue, err := base64.StdEncoding.DecodeString(secretValue)
+			if err != nil {
+				logrus.Error("Error occurred when decoding base64 secret. %w", err)
+				return task.Respond(secretResponse)
+			}
+			secretValue = string(decodedValue)
+		}
+		secretResponse.Value = secretValue
 	}
 	return task.Respond(secretResponse)
 }
 
 type StaticSecret struct {
-	Id    string `json:"id"`
-	Value string `json:"value"`
+	Id     string `json:"id"`
+	Value  string `json:"value"`
+	Base64 bool   `json:"base64"`
 }
 
 type StaticSecretSpec struct {
