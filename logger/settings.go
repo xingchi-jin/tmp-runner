367b080c3c84db72d22f3561c0c39d73d8c6c9f7
Akash Shrivastava <akash.shrivastava@harness.io>
2024-12-12 05:05:54 +0000

    feat: [PIPE-23695]: Adding full filepath in logs (#51)
    * feat: [PIPE-23695]: making getCaller private function
    
    Signed-off-by: Akash Shrivastava <akash.shrivastava@harness.io>
    * fix: [PIPE-23695]: fixed logger interface to show the correct caller
    * feat: [PIPE-23695]: adding relativePath in the logs
    
    Signed-off-by: Akash Shrivastava <akash.shrivastava@harness.io>
    * fix: [PIPE-23695]: fixing for the correct caller in the logs
    
    Signed-off-by: Akash Shrivastava <akash.shrivastava@harness.io>
    * feat: [PIPE-23695]: Adding full filepath in logs
    
    Signed-off-by: Akash Shrivastava <akash.shrivastava@harness.io>

--- logger/settings.go
+++ logger/settings.go
@@ -1,7 +1,9 @@
 package logger
 
 import (
+	"os"
 	"path"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"time"
@@ -26,12 +28,29 @@ func ConfigureLogging(debug, trace bool) {
 	if trace {
 		logrus.SetLevel(logrus.TraceLevel)
 	}
+	AddHook(&CorrectCallerHook{})
 	SetReportCaller(true)
 	SetFormatter(&logrus.JSONFormatter{
 		TimestampFormat: time.RFC3339,
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
-			fileName := path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)
-			return "", fileName
+			return "", getCallerRelativePath(frame)
 		},
 	})
+}
+
+// getCallerRelativePath returns the relative filepath of the caller
+func getCallerRelativePath(frame *runtime.Frame) string {
+	// Get the filename from the current frame
+	fileName := path.Clean(frame.File) + ":" + strconv.Itoa(frame.Line)
+	// Get the base path and find the relative path
+	// if not found, return the absolute path
+	basePath, err := os.Getwd()
+	if err != nil {
+		return fileName
+	}
+	relativePath, err := filepath.Rel(basePath, fileName)
+	if err != nil {
+		return fileName
+	}
+	return relativePath
 }
